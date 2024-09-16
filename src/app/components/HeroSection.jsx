046e5addbf1dacfbb23dfdcf631845ebb5e1f3ca
DavidArinze-Dev <davidarinze4excellence@gmail.com>
2024-09-16 12:42:41 +0100

    first commit

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -22,13 +22,13 @@ const HeroSection = () => {
             <br></br>
             <TypeAnimation
               sequence={[
-                "Judy",
+                "David",
                 1000,
                 "Web Developer",
                 1000,
-                "Mobile Developer",
+                "QA Engineer",
                 1000,
-                "UI/UX Designer",
+                "Test Analyst",
                 1000,
               ]}
               wrapper="span"
